472b384ea12190cfa526e17760761228d3644406
vdeepika <vdeepika@amazon.com>
2020-02-18 13:07:42 -0800

    Challenge 9 solution

---
+++ set2/challenge9/pkcs_padding.py
@@ -0,0 +1,13 @@
+from commons import pkcs_padding
+
+BLOCK_LENGTH = 20
+UNPADDED_TEXT = "YELLOW SUBMARINE"
+
+
+def main():
+    padded_text = pkcs_padding(UNPADDED_TEXT, BLOCK_LENGTH)
+    print(padded_text)
+
+
+if __name__ == "__main__":
+    main()
